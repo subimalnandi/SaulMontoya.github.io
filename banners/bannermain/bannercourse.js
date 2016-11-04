551a7f3c21233adf43a4be78d1f93b58273ac7d0
Saul Montoya <saulmontoya@gidahatari.com>
2016-11-04 10:42:40 -0500

    Initial commit

--- banners/bannermain/bannercourse.js
+++ banners/bannermain/bannercourse.js
@@ -1,53 +1,24 @@
-
 var arr =
 [
-{
-"titulosmall": "Curso de",
-"titulolarge": "Python en Hidrología",
-"lugar": "Lima",
-"fecha": "10-12 Jun",
-"figurafondo": "http://saulmontoya.github.io/banners/bannermain/python.png",
-"url": "http://gidahatari.com/cu-dt/curso-de-python-en-hidrologia-lima-10-12-junio-2016",
-},
-{
-"titulosmall": "Webminar de",
-"titulolarge": "Python Básico",
-"lugar": "Online",
-"fecha": "18-19 May",
-"figurafondo": "http://saulmontoya.github.io/banners/bannermain/python.png",
-"url": "http://gidahatari.com/ht-es/taller-online-de-python-basico",
-},
-{
-"titulosmall": "Curso de",
-"titulolarge": "SIG en la Gestión del Agua",
-"lugar": "Lima",
-"fecha": "8-10 Jul",
-"figurafondo": "http://saulmontoya.github.io/banners/bannermain/gis.png",
-"url": "http://gidahatari.com/cu-dt/curso-de-sig-en-la-gestion-del-agua-lima-8-10-jul-2016",
-},
-{
-"titulosmall": "Curso de",
-"titulolarge": "Modelamiento de Acuíferos con MODFLOW y Model Muse",
-"lugar": "Lima",
-"fecha": "20-22 May",
-"figurafondo": "http://saulmontoya.github.io/banners/bannermain/modelamiento.png",
-"url": "http://gidahatari.com/cu-dt/curso-de-modelamiento-de-acuiferos-con-modflow-y-model-muse-lima-13-15-may-2016"
-}
-];
+{"link": "http://www.gidahatari.com/cu-dt/curso-online-de-manejo-e-interpretacion-de-datos-de-qumica-del-agua", "title": "Curso presencial y online de Manejo e Interpretaci\u00f3n de Datos de Qu\u00edmica del Agua. 5, 7, 9, 13, 15 y 16 Dic 2016"},
+{"link": "http://www.gidahatari.com/cu-dt/curso-de-modelamiento-de-acuiferos-con-modflow-y-model-muse-lima-16-18-dic-2016", "title": "Curso presencial y online de Modelamiento de Acu\u00edferos con MODFLOW y Model Muse, 16-18 Dic 2016"},
+{"link": "http://www.gidahatari.com/cu-dt/curso-presencial-y-online-de-modelacion-hidrologica-en-la-gestion-de-recursos-hidricos-con-hec-hms-y-hec-ressim", "title": "Curso presencial y online de Modelamiento Hidrol\u00f3gico en la Gesti\u00f3n del Agua con HEC-HMS y HEC-ResSim. 13-15 Ene 2017"}
+]
 
     var tit_small = "";
     var tit_large = "";
     var c_lug = "";
     var c_fec = "";
-    var fig_fon = "";
+    var fig_fon = Math.floor((Math.random() * 8)) + '.jpg';
+    console.log(fig_fon);
     var lin_url = "";
     var i = Math.floor((Math.random() * arr.length));
-    tit_small += arr[i].titulosmall;
-    tit_large += arr[i].titulolarge;
-    c_lug += arr[i].lugar;
-    c_fec += arr[i].fecha;
-    fig_fon += arr[i].figurafondo;
-    lin_url += arr[i].url;
+    //tit_small += arr[i].titulosmall;
+    tit_large += arr[i].title;
+    //c_lug += arr[i].lugar;
+    //c_fec += arr[i].fecha;
+    //fig_fon += arr[i].figurafondo;
+    lin_url += arr[i].link;
     document.getElementById("cutitulosmall").innerHTML = tit_small;
     document.getElementById("cutitulolarge").innerHTML = tit_large;
     document.getElementById("culugar").innerHTML = c_lug;
