1149df8ed6413a47abca5a409d3a94d81ff81507
Saul Montoya <saulmontoya@gidahatari.com>
2016-11-04 12:04:01 -0500

    Initial commit

---
+++ banners/bannermain/bannercourse_backup.js
@@ -0,0 +1,23 @@
+
+
+    var tit_small = "";
+    var tit_large = "";
+    var c_lug = "";
+    var c_fec = "";
+    var fig_fon = Math.floor((Math.random() * 5)) + '.png';
+    console.log(fig_fon);
+    var lin_url = "";
+    var i = Math.floor((Math.random() * arr.length));
+    //tit_small += arr[i].titulosmall;
+    tit_large += arr[i].title;
+    //c_lug += arr[i].lugar;
+    //c_fec += arr[i].fecha;
+    //fig_fon += arr[i].figurafondo;
+    lin_url += arr[i].link;
+    document.getElementById("cutitulosmall").innerHTML = tit_small;
+    document.getElementById("cutitulolarge").innerHTML = tit_large;
+    document.getElementById("culugar").innerHTML = c_lug;
+    document.getElementById("cufecha").innerHTML = c_fec;
+    document.getElementById("cufigurafondo").innerHTML = '<img src=' + fig_fon + ' class="cub-imgcur cub-gen-imgcur-animation" >';
+    document.getElementById("cucontenidolink").innerHTML = '<a href=' + lin_url + ' style="text-decoration: none"><span class="cub-div-fcontent cub-contbold">Contenido</span></a>'
+    document.getElementById("cuinscripcionlink").innerHTML = '<a href=' + lin_url + ' style="text-decoration: none"><span class="cub-div-fcontent cub-contbold">Inscripción</span></a>'
