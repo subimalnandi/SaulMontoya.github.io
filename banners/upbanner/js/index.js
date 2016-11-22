300be4a0f17ef8180d3f19260a8db469f331c641
Saul Montoya <saulmontoya@gidahatari.com>
2016-11-22 08:02:05 +0000

    Initial commit

--- banners/upbanner/js/index.js
+++ banners/upbanner/js/index.js
@@ -1,9 +1,5 @@
 var arr = 
-[
-{"link": "http://www.gidahatari.com/cu-dt/curso-online-de-manejo-e-interpretacion-de-datos-de-qumica-del-agua", "title": "Curso presencial y virtual de Manejo e Interpretaci\u00f3n de Datos de Qu\u00edmica del Agua. 5, 7, 9, 13, 14 y 15 Dic 2016"},
-{"link": "http://www.gidahatari.com/cu-dt/curso-de-modelamiento-de-acuiferos-con-modflow-y-model-muse-lima-16-18-dic-2016", "title": "Curso presencial y online de Modelamiento de Acu\u00edferos con MODFLOW y Model Muse, 16-18 Dic 2016"},
-{"link": "http://www.gidahatari.com/cu-dt/curso-presencial-y-online-de-modelacion-hidrologica-en-la-gestion-de-recursos-hidricos-con-hec-hms-y-hec-ressim", "title": "Curso presencial y online de Modelamiento Hidrol\u00f3gico en la Gesti\u00f3n del Agua con HEC-HMS y HEC-ResSim. 13-15 Ene 2017"}
-]
+
 
 var i = Math.floor((Math.random() * arr.length));
 
