ca6d87a5aca18e0080b20faaa42dc87d1f88aacd
Saul Montoya <saulmontoya@gidahatari.com>
2016-12-06 08:02:05 +0000

    Initial commit

--- banners/bannermain/bannercourse.js
+++ banners/bannermain/bannercourse.js
@@ -1,7 +1,6 @@
 var arr = 
 [
 {"link": "http://www.gidahatari.com/ht-es/taller-de-geofisica-de-campo-para-evaluacion-de-la-intrusion-salina", "title": "Taller de Geof\u00edsica de Campo para Evaluaci\u00f3n de la Intrusi\u00f3n Salina"},
-{"link": "http://www.gidahatari.com/cu-dt/curso-online-de-manejo-e-interpretacion-de-datos-de-qumica-del-agua", "title": "Curso presencial y virtual de Manejo e Interpretaci\u00f3n de Datos de Qu\u00edmica del Agua. 5, 7, 9, 13, 14 y 15 Dic 2016"},
 {"link": "http://www.gidahatari.com/cu-dt/curso-presencial-y-online-de-modelacion-hidrologica-en-la-gestion-de-recursos-hidricos-con-hec-hms-y-hec-ressim", "title": "Curso presencial y online de Modelamiento Hidrol\u00f3gico en la Gesti\u00f3n del Agua con HEC-HMS y HEC-ResSim. 13-15 Ene 2017"}
 ]
 
